c2325ed85461216eefe66545cb97c350b3d9286a
a.jungchan <89140961+AN-Jung-Chan@users.noreply.github.com>
2022-06-18 21:28:18 +0900

    강사님꺼 복습

--- TheEssenceofJava/src/Chapter02/Ex06.java
+++ TheEssenceofJava/src/Chapter02/Ex06.java
@@ -2,6 +2,19 @@ package Chapter02;
 
 public class Ex06 {
 	public static void main(String[] args) {
-		System.out.println("하하 하하하하하");
+		String str = "Hello";
+		System.out.println(str);
+
+		System.out.println("--------------------------");
+
+		byte a1 = 10;
+		int b1 = a1; // Promortion (자동)
+
+		int a2 = 10;
+		//byte b2 = a; // casting (명시)
+
+		long c = 12;
+		//float d = 3.14; // 3.14f 또는 (float)3.14
+
 	}
 }
