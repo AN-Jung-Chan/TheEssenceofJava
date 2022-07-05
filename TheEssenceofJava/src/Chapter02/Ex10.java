30b1e9beaae375bb1c0b58d6dcbca13c2d49562a
a.jungchan <89140961+AN-Jung-Chan@users.noreply.github.com>
2022-07-05 22:14:22 +0900

    연산자

--- TheEssenceofJava/src/Chapter02/Ex10.java
+++ TheEssenceofJava/src/Chapter02/Ex10.java
@@ -1,7 +1,34 @@
 package Chapter02;
-
+//연산자 쉽게 이해하기!
 public class Ex10 {
 	public static void main(String[] args) {
+		
+		int i = 0;
+		int j = 10;
+		i++;
+		j--;
+		
+		System.out.println(i);
+		System.out.println(j);
+		
+		// i++ 값이 참조된 후에 증가
+		// ++i 값이 참조되기 전에 증가
+		int k = 0;
+		int l = 0;
+		System.out.println(++k);
+		System.out.println(l++);
+		System.out.println(l);
+		
+		System.out.println("-------------------------------------");
+		// ! 논리부정 - 내용의 반대되는 값을 표현, boolean에서만 사용가능
+		boolean bool1 = false;
+		System.out.println("a = " + bool1);
+		System.out.println("b = " + !bool1);
+		
+		boolean bool2 = 5 > 3;
+		System.out.println("c = " + bool2);
+		System.out.println("d = " + !(5 > 3));
+		
 		
 	}
 }
