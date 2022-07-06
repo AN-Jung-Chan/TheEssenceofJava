14d430effb9db6afffb3d725667fa0c138afc871
a.jungchan <89140961+AN-Jung-Chan@users.noreply.github.com>
2022-07-06 22:05:44 +0900

    삼항연산자 정리 및 예제

--- TheEssenceofJava/src/Chapter02/Ex11.java
+++ TheEssenceofJava/src/Chapter02/Ex11.java
@@ -1,8 +1,40 @@
 package Chapter02;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Ex11 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		// 삼항연산자 : 조건항? 항1 : 항2
+		// 조건항이 true면 항1을 실행하고 조건항이 false면 항2를 실행한다.
+
+		// 예제1 삼항연산자를 if문으로 풀어서 설명
+		String grade1 = "";
+		int math1 = 70;
+		if (math1 >= 90) {
+			grade1 = "pass";
+		} else {
+			grade1 = "fail";
+		}
+		System.out.println(math1 + "는 " + grade1);
+		System.out.println();
+
+		// 예제1을 삼항연산자로 할 시 - 변수 = (조건) ? 참 : 거짓; <<<< 꼭 기억!!
+		int math2 = 70;
+		String grade2 = (math2 >= 90) ? "pass" : "fail";
+		System.out.println(math2 + "는 " + grade2);
+
+		System.out.println();
+
+		// BufferedReader 를 이용하여 값을 입력받아 출력하는 예제
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("선택하시오.");
+		System.out.print("1.남자 2.여자 = ");
+		int x = Integer.parseInt(in.readLine());
+		String str = x == 1 ? "남성" : "여성";
+		System.out.println("str = " + str);
 
 	}
 }
