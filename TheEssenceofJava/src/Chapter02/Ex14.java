57a54899a5db1804b421cec9b495db4b8f7f9f12
a.jungchan <89140961+AN-Jung-Chan@users.noreply.github.com>
2022-07-08 23:00:36 +0900

    switch ~ case 구문 정리 및 예제

--- TheEssenceofJava/src/Chapter02/Ex14.java
+++ TheEssenceofJava/src/Chapter02/Ex14.java
@@ -1,5 +1,49 @@
 package Chapter02;
 
-public class Ex14 {
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Ex14 {
+	public static void main(String[] args) throws IOException {
+
+//		/* SWITCH ~ CASE 구문
+//		 * switch : 바꾸다 / case : 상황, 경우
+//		 * 조건에 들어온 값에 따라 case 상수에 매치되는 줄부터 실행시키는 제어문(흐름 분기)  
+//		 * 
+//		 * 옵션구문
+//		 * break : 현재 실행중인 switch 영역을 나가서 다음 문장을 실행하도록 한다.
+//		 * default : 조건에 들어온 값에 해당하는 case가 없을 때 찾는 case (if문의 else 같은..?)
+//		 */
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//		System.out.print("숫자 하나를 입력하시오 : ");
+//		int a = Integer.parseInt(br.readLine());
+//
+//		switch (a) {	// 입력된 숫자 조건이 반영
+//		case 1:			// 같은 숫자가 나오면  
+//			System.out.println("1입력");	// 해당 코드 실행
+//			break;		//실행 완료 후 프로그램 종료.
+//		case 2:
+//			System.out.println("2입력");
+//			break;
+//		case 3:
+//			System.out.println("3입력");
+//			break;
+//		default:
+//			System.out.println("1, 2, 3 이외의 숫자 입력함");
+//		}
+//		
+//		/* 예제 - 예제 주민등록번호의 7번째 자리 수를 이용하여 출생년도를 파악하시오
+//		 * -break의 사용은 필수가 아니라는 것을 이해하기 위한 예제이다.-
+//		 */
+		
+
+		
+		
+		
+		
+		
+	}
 }
