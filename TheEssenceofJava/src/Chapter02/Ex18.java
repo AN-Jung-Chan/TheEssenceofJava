9f55b49a70ca7b6c4934e6453d3d9d0ea1cfa5a8
a.jungchan <89140961+AN-Jung-Chan@users.noreply.github.com>
2022-07-14 23:10:28 +0900

    중첩 for문 예제17개 / 별만들기 연습,

--- TheEssenceofJava/src/Chapter02/Ex18.java
+++ TheEssenceofJava/src/Chapter02/Ex18.java
@@ -103,8 +103,8 @@ public class Ex18 {
 //			System.out.println();
 //		}
 //		
-		System.out.println();
-		System.out.println("-- 예제 7 -- ");
+//		System.out.println();
+//		System.out.println("-- 예제 7 -- ");
 //		/*
 //		 * 56789
 //		 * 45678
@@ -112,10 +112,88 @@ public class Ex18 {
 //		 * 23456
 //		 * 12345
 //		 */
-
-		for (int i = 6; i >= 2; i--) {
-			for (int j = i - 1; j <= i + 3; j++) {
-				System.out.print(j);
+//
+//		for (int i = 6; i >= 2; i--) {
+//			for (int j = i - 1; j <= i + 3; j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		
+//		
+//		System.out.println();
+//		System.out.println("-- 예제 8 -- ");
+//		/*
+//		 * * 
+//		 * ** 
+//		 * *** 
+//		 * **** 
+//		 * *****
+//		 */
+//
+//		for (int i = 0; i < 5; i++) {
+//			for (int j = 0; j <= i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//
+//		System.out.println();
+//		System.out.println("-- 예제 9 -- ");
+//		/*
+//		 * *****
+//		 * ****
+//		 * ***
+//		 * **
+//		 * *
+//		 */
+//		
+//		for (int i = 5; i >= 1; i--) {
+//			for (int j = 1; j <= i ; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		
+//		System.out.println();
+//		System.out.println("-- 예제 10 -- ");
+//		/*
+//		 * *****
+//		 *  ****
+//		 *   ***
+//		 *    **
+//		 *     *
+//		 */
+//		
+//		
+//		for(int i = 0; i < 5; i++) {  // 5줄 행
+//			for (int j = 0; j < i; j++) { // 띄어쓰기 i
+//				System.out.print(" ");
+//			}
+//			
+//			for(int k = 5; k > i; k--) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		
+//		System.out.println();
+		System.out.println("-- 예제 10 -- ");
+		/*
+		 *     *
+		 *    ** 
+		 *   *** 
+		 *  ****     
+		 * *****      
+		 */
+		
+		for(int i = 1; i < 6; i++) {  // 5줄 행
+			for (int j = 5; j > i; j--) { // 띄어쓰기 
+				System.out.print(" ");
+			}
+			
+			for(int k = 1; k < i+1; k++) { // 별 
+				System.out.print("*");
 			}
 			System.out.println();
 		}
@@ -131,9 +209,14 @@ public class Ex18 {
 		
 		
 		
-
-//		System.out.println();
-//		System.out.println("-- 예제 6 -- ");
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 }
