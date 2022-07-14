90c8fe380e2770426b900815af3961b9a6c95751
a.jungchan <89140961+AN-Jung-Chan@users.noreply.github.com>
2022-07-14 22:46:43 +0900

    중첩for문 정리 및 예제 // ex) 바깥for문은 외부반복(i, 행, 세로) 안쪽for문은 내부반복(j, 열, 가로)

--- TheEssenceofJava/src/Chapter02/Ex16.java
+++ TheEssenceofJava/src/Chapter02/Ex16.java
@@ -5,21 +5,18 @@ public class Ex16 {
 		
 		/*	중첩 for 구문
 		 *	for문 안에 for문을 사용하는 형식 
-		 *	바깥for문은 외부반복(열,세로), 안쪽for문은 내부반복(행,가로)이다.
+		 *	바깥for문은 외부반복(행,세로), 안쪽for문은 내부반복(열,가로)이다.
 		 */
 		
 		System.out.println("--예제--");
 		
 		int sum = 0;
-		for (int i = 0; i < 10; i++) { // 외부반복, 열, 세로
-			for ( int j = 0; j < 10; j++) { // 내부반복, 행, 가로
+		for (int i = 0; i < 10; i++) { // 외부반복, 행, 세로
+			for ( int j = 0; j < 10; j++) { // 내부반복, 열, 가로
 				sum += 1;	
-//				System.out.println(sum);
-//				System.out.print("i = " + i + "/ " ); 	// 외부반복, 열, 세로
-//				System.out.print("j = " + j + "/ " );	// 내부반복, 행, 가로
-//				System.out.print("i = " + i + ", j = " + j + " / " ); //확인용
+				System.out.print(sum + " / ");
 			}
-//			System.out.println();
+			System.out.println();
 		}
 		
 		System.out.println("sum = " + sum);
