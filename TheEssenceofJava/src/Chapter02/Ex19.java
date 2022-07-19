ff9b1d3628ddaf7e4ceb32efef343cd954643759
a.jungchan <89140961+AN-Jung-Chan@users.noreply.github.com>
2022-07-19 23:28:46 +0900

    여기서부터 ㄱㄱ

--- TheEssenceofJava/src/Chapter02/Ex19.java
+++ TheEssenceofJava/src/Chapter02/Ex19.java
@@ -1,7 +1,16 @@
 package Chapter02;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Ex19 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("안녕");
+		String re = br.readLine();
+		System.out.println(re);
 	}
 }
