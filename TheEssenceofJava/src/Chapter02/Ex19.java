18a801fc0f119ca3b3415442524d72093a9bdfbe
a.jungchan <89140961+AN-Jung-Chan@users.noreply.github.com>
2022-07-25 22:25:28 +0900

    do ~ while 구문 정리 및 예제, 점수 산정프로그램

--- TheEssenceofJava/src/Chapter02/Ex19.java
+++ TheEssenceofJava/src/Chapter02/Ex19.java
@@ -7,9 +7,7 @@ import java.io.InputStreamReader;
 public class Ex19 {
 	public static void main(String[] args) throws IOException {
 		/*
-		 * while 구문 
-		 * 프로그램 코드의 특정 부분을 반복하여 수행한다. 
-		 * 조건이 참이면 실행하고 다시 조건을 확인한다.
+		 * while 구문 프로그램 코드의 특정 부분을 반복하여 수행한다. 조건이 참이면 실행하고 다시 조건을 확인한다.
 		 */
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -56,35 +54,44 @@ public class Ex19 {
 //		 * 최소 한번은 내용이 실행된다.
 //		 */
 //		
-//		int kor1 = 0;
-//		
-//		while(true) {
-//			System.out.print("국어 점수 입력 : ");
-//			kor1 = Integer.parseInt(br.readLine());
-//			if (kor1 > 100 || kor1 <= 0)
-//				System.out.println("1~100점 범위를 벗어났습니다.");
-//			else
-//				break;
-//		}
-//		
-//		System.out.println("-- 예제3 --");
-//		System.out.println();
-		
-		int kor2 = 0;
-		
-		do {
+		int kor1 = 0;
+		while (true) {
 			System.out.print("국어 점수 입력 : ");
-			kor2 = Integer.parseInt(br.readLine());
-		} while ( kor2 > 100 || kor2 <= 0);
+			kor1 = Integer.parseInt(br.readLine());
+
+			if (kor1 <= 100 && kor1 >= 90)
+				System.out.println("입력하신 점수는 " + kor1 + "점 이며, 등급은 'A'입니다.");
+			else if (kor1 < 90 && kor1 >= 80)
+				System.out.println("입력하신 점수는 " + kor1 + "점 이며, 등급은 'B'입니다.");
+			else if (kor1 < 80 && kor1 >= 70)
+				System.out.println("입력하신 점수는 " + kor1 + "점 이며, 등급은 'C'입니다.");
+			else if (kor1 < 70 && kor1 >= 60)
+				System.out.println("입력하신 점수는 " + kor1 + "점 이며, 등급은 'D'입니다.");
+			else if (kor1 < 60 && kor1 >= 50)
+				System.out.println("입력하신 점수는 " + kor1 + "점 이며, 등급은 'E'입니다.");
+			else if (kor1 < 50 && kor1 >= 0)
+				System.out.println("입력하신 점수는 " + kor1 + "점 이며, '불합격' 입니다.");
+			else if (kor1 > 100 || kor1 < 0)
+				System.out.println("1~100점 범위를 벗어났습니다. 다시 입력하세요.");
 			
-		
-		
-		
-		
-		
-		
-		
-		
-		
+				System.out.print("계속 하시겠습니다?(y/n) : ");
+			char ch = (char) System.in.read();
+			System.out.println();
+			System.in.read();
+			System.in.read();
+			if (ch == 'n' || ch == 'N') // n 또는 N이 입력되면 멘트 후 프로그램 종료.
+				System.out.println("프로그램을 종료합니다.");
+			break;
+
+		}
+//		
+//		//do ~ while 구문 사용
+//		int kor2 = 0;
+//		
+//		do {
+//			System.out.print("국어 점수 입력 : ");
+//			kor2 = Integer.parseInt(br.readLine());
+//		} while ( kor2 > 100 || kor2 <= 0);
+
 	}
 }
