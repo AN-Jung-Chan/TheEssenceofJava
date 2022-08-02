5a82b7f0430539dc654174fa7c002292aa5e6d66
a.jungchan <89140961+AN-Jung-Chan@users.noreply.github.com>
2022-08-02 18:33:29 +0900

    java 메서드 정리, 많이 사용하는 문법이니 꼭 숙지하기.

--- TheEssenceofJava/src/Chapter02/Ex21.java
+++ TheEssenceofJava/src/Chapter02/Ex21.java
@@ -1,5 +1,50 @@
 package Chapter02;
 
+import java.io.IOException;
+
 public class Ex21 {
+//	/* 메서드 형식 
+//	 * 접근제한자 [지정예약어] *반환자료형 *메서드명 *(매개변수들) [throws 예외클래스들] 
+//	 * { 
+//	 * 내용; 
+//	 * 내용; 
+//	 * ... 
+//	 * 내용;
+//	 * return // 필요시 사용 }
+//	 */
+	
+	public static void main(String[] args) throws IOException { // 순서 1
+		int ret = add(1, 2);
+		System.out.println("ret : " + ret);
+	}
+
+	public static int add(int a, int b) { // 순서 2
+		int sum = a + b;
+		return sum;
+		
+	}
+	/* 코드 설명 - 위 예제를 실행하면 다음과 같은 절차로 실행된다. **순서가 바뀌어도 코드 실행에 문제가 없다. 편한대로 하면된다.
+	 * 1. main 메서드 실행
+	 * 2. 1과 2값을 add메서드의 매개변수에 복사하며 호출
+	 * 3. add메서드 실행
+	 * 4. 매개변수 a와 b값을 더해서 sum에 대입
+	 * 5. sum의 값을 호출했던 곳으로 돌려주고(return) add메서드 종료
+	 * 6. add메서드로부터 반환 받은 sum의 값을 ret변수에 대입
+	 * 7. System.out.println으로 결과 ret변수의 값을 출력
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
