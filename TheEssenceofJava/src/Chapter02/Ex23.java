65e861be06241e4d8060d3cf11524f869cb44d81
a.jungchan <89140961+AN-Jung-Chan@users.noreply.github.com>
2022-08-02 23:37:34 +0900

    배열 정리 및 예제

--- TheEssenceofJava/src/Chapter02/Ex23.java
+++ TheEssenceofJava/src/Chapter02/Ex23.java
@@ -1,5 +1,95 @@
 package Chapter02;
 
+import java.io.IOException;
+
+
 public class Ex23 {
+	public static void main(String[] args) throws IOException {
+//		/* 배열
+//		 * 1차원 배열 선언(배열 참조 변수)
+//		 * int[] arr; //참조변수
+//		 * int arr[]; //참조변수
+//		 * arr = new int[4];
+//		 * 배열은 new를 통해 생성(new 연산자는 동적 할당 가능)
+//		 * java의 Garbege Collector가 Garbege Collection Heap메모리에 할당
+//		 * G.C가 자동 초기화한다.
+//		 */
+//		
+//		int[] arr1 = {1, 2, 3}; //선언 시만 사용		arr1 ㅡ> 1 2 3
+//		System.out.println("arr1[0] : " + arr1[0]);
+//		System.out.println("arr1[1] : " + arr1[1]);
+//		System.out.println("arr1[2] : " + arr1[2]);
+//		int[] arr2, arr3;		// 				arr2 ㅡ> arr3 ㅡ>
+//		arr2 = new int[3];		// heap에 할당		arr2 ㅡ> 0 0 0
+//		arr2[0]	= 4;			//				arr2 ㅡ> 4 0 0
+//		arr2[1]	= 5;			//				arr2 ㅡ> 4 5 0
+//		arr2[2]	= 6;			//				arr2 ㅡ> 4 5 6
+//		arr3 = new int[] {7, 8, 9}; //heap에 할당 및 초기화 	arr3 ㅡ> 7 8 9
+//
+//		System.out.println();
+//		System.out.println("-- 예제 1 --");
+//
+//		char[] str = { 'a', 'b', 'c' }; // 배열생성 및 초기화
+//		
+//		System.out.println("str[0] : " + str[0]);
+//		System.out.println("str[1] : " + str[1]);
+//		System.out.println("str[2] : " + str[2]);
+//		
+//		System.out.println();
+//		System.out.println("-- 예제 2 --");
+//
+//		int[] arr; // 배열참조변수 생성
+//		arr = new int[] { 10, 20, 30, 40 }; // 배열 생성 및 초기화
+//
+//		System.out.println("arr[0] : " + arr[0]);
+//		System.out.println("arr[1] : " + arr[1]);
+//		System.out.println("arr[2] : " + arr[2]);
+//		System.out.println("arr[3] : " + arr[3]);
+//		
+//		System.out.println();
+//		System.out.println("-- 예제 3 --");
+//		// for문 이용하기
+//		int[] arr;
+//		arr = new int[5];
+//
+//		for (int i = 0; i < 5; i++) {
+//			arr[i] = i + 1; // 배열 요소에 값 대입
+//		}
+//
+//		for (int i = 0; i < arr.length; i++) { // arr.length == 5
+//			System.out.println("arr[" + i + "] : " + arr[i]);
+//		}
+//		
+//		System.out.println();
+//		System.out.println("-- 예제3 foreach형식 --");
+//		/* 아래의 for문은 타 언어에서 foreach라고도 불리는 형식이다.
+//		 * arr에 있는 요소를 순회하며 하나의 값을 앞의 변수에 담고 값이 있으면 종속문장을 실행한다.
+//		 * 즉, arr의 요소의 개수만큼 for문을 반복 시킬 수 있음.
+//		 * 
+//		 */
+//		int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+//		int i = 0;
+//		for (int a : arr) {
+//			System.out.println("arr[" + i + "] : " + a);
+//			i++;
+//		}
+
+//		System.out.println();
+//		System.out.println("-- 예제문제 성적구현 --");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 
 }
